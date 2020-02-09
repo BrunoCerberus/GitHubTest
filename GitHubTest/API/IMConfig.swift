4c982dd9919a24c6945514cf44819d54c751a276
Bruno Lopes de Mello <tqi_bmello@uolinc.com>
2020-02-09 17:25:42 -0300

    removed code's chunk from the main method becuse it triggered a warning

--- GitHubTest/API/IMConfig.swift
+++ GitHubTest/API/IMConfig.swift
@@ -41,46 +41,8 @@ class IMConfig<T: Fetcher> {
             request.httpBody = httpBody
         }
         
-        print("↗️ ======= REQUEST =======")
-        print("↗️ REQUEST: \(urlRequest)")
-        print("↗️ HTTP METHOD: \(method)")
-        if let requestHeaders = request.allHTTPHeaderFields,
-            let requestHeadersData = try? JSONSerialization.data(withJSONObject: requestHeaders, options: .prettyPrinted),
-            let requestHeadersString = String(data: requestHeadersData, encoding: .utf8) {
-            print("↗️ HEADERS:\n\(requestHeadersString)")
-        }
-        
-        if let requestBodyData = request.httpBody,
-            let requestBody = String(data: requestBodyData, encoding: .utf8) {
-            print("↗️ BODY: \n\(requestBody)")
-        }
-        
         session.dataTask(with: request) { (dataRequest, response, error) in
-            if let httpResponse = response as? HTTPURLResponse {
-                print("\n↙️ ======= RESPONSE =======")
-                switch httpResponse.statusCode {
-                case 200...202:
-                    print("↙️ CODE: \(httpResponse.statusCode) - ✅")
-                case 400...505:
-                    print("↙️ CODE: \(httpResponse.statusCode) - 🆘")
-                default:
-                    print("↙️ CODE: \(httpResponse.statusCode) - ✴️")
-                }
-                
-                if let responseHeadersData = try? JSONSerialization.data(withJSONObject: httpResponse.allHeaderFields,
-                                                                         options: .prettyPrinted),
-                    let responseHeadersString = String(data: responseHeadersData, encoding: .utf8) {
-                    print("↙️ HEADERS:\n\(responseHeadersString)")
-                }
-                
-                if let responseBodyData = dataRequest,
-                    let responseBody =  String(data: responseBodyData, encoding: .utf8),
-                    !responseBody.isEmpty {
-                    
-                    print("↙️ BODY:\n\(responseBody)\n")
-                }
-            }
-            
+            self.debugResponse(request, dataRequest, response, error)
             guard let data = dataRequest else {
                 print("\n\n===========Error===========")
                 print("Error Code: \(error?._code ?? 0)")
@@ -106,5 +68,61 @@ class IMConfig<T: Fetcher> {
                 return
             }
         }.resume()
+    }
+}
+
+private extension IMConfig {
+    private func debugResponse(_ request: URLRequest,
+                               _ responseData: Data?,
+                               _ response: URLResponse?,
+                               _ error: Error?) {
+        let uuid = UUID().uuidString
+        print("\n↗️ ======= REQUEST =======")
+        print("↗️ REQUEST #: \(uuid)")
+        print("↗️ URL: \(request.url?.absoluteString ?? "")")
+        print("↗️ HTTP METHOD: \(request.httpMethod ?? "GET")")
+        
+        if let requestHeaders = request.allHTTPHeaderFields,
+            let requestHeadersData = try? JSONSerialization.data(withJSONObject: requestHeaders, options: .prettyPrinted),
+            let requestHeadersString = String(data: requestHeadersData, encoding: .utf8) {
+            print("↗️ HEADERS:\n\(requestHeadersString)")
+        }
+        
+        if let requestBodyData = request.httpBody,
+            let requestBody = String(data: requestBodyData, encoding: .utf8) {
+            print("↗️ BODY: \n\(requestBody)")
+        }
+        
+        if let httpResponse = response as? HTTPURLResponse {
+            print("\n↙️ ======= RESPONSE =======")
+            switch httpResponse.statusCode {
+            case 200...202:
+                print("↙️ CODE: \(httpResponse.statusCode) - ✅")
+            case 400...505:
+                print("↙️ CODE: \(httpResponse.statusCode) - 🆘")
+            default:
+                print("↙️ CODE: \(httpResponse.statusCode) - ✴️")
+            }
+            
+            if let responseHeadersData = try? JSONSerialization.data(withJSONObject: httpResponse.allHeaderFields, options: .prettyPrinted),
+                let responseHeadersString = String(data: responseHeadersData, encoding: .utf8) {
+                print("↙️ HEADERS:\n\(responseHeadersString)")
+            }
+            
+            if let responseBodyData = responseData,
+                let responseBody =  String(data: responseBodyData, encoding: .utf8),
+                !responseBody.isEmpty {
+                
+                print("↙️ BODY:\n\(responseBody)\n")
+            }
+        }
+        
+        if let urlError = error as? URLError {
+            print("\n❌ ======= ERROR =======")
+            print("❌ CODE: \(urlError.errorCode)")
+            print("❌ DESCRIPTION: \(urlError.localizedDescription)\n")
+        }
+        
+        print("======== END OF: \(uuid) ========\n\n")
     }
 }
